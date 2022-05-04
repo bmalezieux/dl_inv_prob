3ec6ff36898c2de0ea8a42a7ab0774bfb6e4a266
Benoit Malezieux <bmalezie@drago5.saclay.inria.fr>
2022-05-04 18:58:41 +0200

    FIX test + algo

--- dl_inv_prob/tests/test_dl.py
+++ dl_inv_prob/tests/test_dl.py
@@ -1,34 +1,37 @@
 import numpy as np
+import torch
 
 from dl_inv_prob.dl import DictionaryLearning
 from dl_inv_prob.utils import generate_data, recovery_score, generate_dico
 
 
-RNG = np.random.default_rng(100)
-DEVICE = "cuda:1"
+DEVICE = "cuda:1" if torch.cuda.is_available() else "cpu"
 
 
 def test_run_dl():
-    dl = DictionaryLearning(50, device=DEVICE, rng=RNG)
-    A = RNG.random(size=(10, 30, 50))
-    Y = RNG.random(size=(10, 30, 100))
+    rng = np.random.default_rng(100)
+    dl = DictionaryLearning(50, device=DEVICE, rng=rng)
+    A = rng.random(size=(10, 30, 50))
+    Y = rng.random(size=(10, 30, 100))
     dl.fit(Y, A)
     assert dl.D_.shape == (50, 50)
 
 
 def test_dl_denoising():
-    dico = generate_dico(50, 50, rng=RNG)
-    Y, _ = generate_data(dico[None, :], 1000, rng=RNG)
-    dl = DictionaryLearning(50, lambd=0.1, device=DEVICE, rng=RNG)
+    rng = np.random.default_rng(100)
+    dico = generate_dico(50, 50, rng=rng)
+    Y, _ = generate_data(dico[None, :], 1000, rng=rng)
+    dl = DictionaryLearning(50, lambd=0.1, device=DEVICE, rng=rng)
     dl.fit(Y)
     assert recovery_score(dl.D_, dico) >= 0.99
 
 
 def test_dl_inv_prob():
-    dico = generate_dico(50, 50, rng=RNG)
-    A = RNG.random(size=(1, 50, 50))
+    rng = np.random.default_rng(100)
+    dico = generate_dico(50, 50, rng=rng)
+    A = rng.normal(size=(1, 50, 50))
     A /= np.linalg.norm(A, axis=1, keepdims=True)
-    Y, _ = generate_data(A @ dico, 1000, rng=RNG)
-    dl = DictionaryLearning(50, lambd=0.1, device=DEVICE, rng=RNG)
+    Y, _ = generate_data(A @ dico, 1000, rng=rng)
+    dl = DictionaryLearning(50, lambd=0.1, device=DEVICE, rng=rng)
     dl.fit(Y, A)
     assert recovery_score(dl.D_, dico) >= 0.9

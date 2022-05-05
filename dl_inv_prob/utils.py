72e956b130c4c769548351082ad0940cff03f638
Florent Michel <flomiche@drago3.saclay.inria.fr>
2022-05-05 14:29:57 +0200

    add image processing functions

--- dl_inv_prob/utils.py
+++ dl_inv_prob/utils.py
@@ -68,3 +68,75 @@ def recovery_score(D, Dref):
     score = cost_matrix[row_ind, col_ind].sum() / D.shape[1]
 
     return score
+
+
+def extract_patches(img, dim_patch):
+    """Extract non overlapping square patches from an image in a grid pattern.
+
+    Parameters
+    ----------
+    img : numpy.array, shape (dim_img, dim_img)
+        Square image with a single channel
+    dim_patch : int
+        Dimension of the square patches to extract
+
+    Returns
+    -------
+    patches : numpy.array, shape (nb_patches, dim_patch, dim_patch)
+        3d array containg the extracted patches
+    """
+    dim_img = img.shape[0]
+    nb_patches = (dim_img // dim_patch) ** 2
+    res = np.zeros((nb_patches, dim_patch, dim_patch))
+    i_patch = 0
+    for i in range(0, dim_img, dim_patch):
+        for j in range(0, dim_img, dim_patch):
+            res[i_patch, :, :] = img[i:i + dim_patch, j:j + dim_patch]
+            i_patch += 1
+    patches = np.array(res)
+    
+    return patches
+
+
+def combine_patches(patches):
+    """Combine extracted patches (in a grid fashion) into the original image.
+
+    Parameters
+    ----------
+    patches : numpy.array, shape (nb_patches, dim_patch, dim_patch)
+        3d array containing the extracted patches
+
+    Returns
+    -------
+    img : numpy.array, shape (dim_img, dim_img)
+        original image
+    """
+    n_patches = patches.shape[0]
+    dim_grid = int(np.sqrt(n_patches))
+
+    rows = [np.hstack([patches[i] for i in range(k, k + dim_grid)])
+            for k in range(0, n_patches, dim_grid)]
+    img = np.vstack(rows)
+    
+    return img
+
+
+def psnr(rec, ref):
+    """Compute the peak signal-to-noise ratio for images on 8 bits.
+
+    Parameters
+    ----------
+    rec : numpy.array, shape (height, width)
+        reconstructed image
+    ref : numpy.array, shape (height, width)
+        original image
+
+    Returns
+    -------
+    psnr : float
+        psnr of the reconstructed image
+    """
+    mse = np.square(rec - ref).mean()
+    psnr = 10 * np.log10(255 ** 2 / mse)
+    
+    return psnr
